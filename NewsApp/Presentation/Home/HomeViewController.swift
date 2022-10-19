00b59aa64d09ce7bfb54b6dee02cc82bc78f5c99
Khalil <Khalilp@brq.com>
2022-10-19 14:07:13 -0300

    Create tableView

--- NewsApp/Presentation/Home/HomeViewController.swift
+++ NewsApp/Presentation/Home/HomeViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private var tableView: UITableView = {
+        let view = UITableView()
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        setupUI()
     }
     
     override func viewDidLoad() {
@@ -31,5 +40,41 @@ class HomeViewController: UIViewController {
             }
         }
     }
+    
+    private func setupUI() {
+        view.addSubview(tableView)
+        
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(tableView.topAnchor.constraint(equalTo: view.topAnchor))
+        constraints.append(tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor))
+        constraints.append(tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor))
+        constraints.append(tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor))
+        
+        NSLayoutConstraint.activate(constraints)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 }
 
+//MARK: - UITableViewDelegate, UITableViewDataSource
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
