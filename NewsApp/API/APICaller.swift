c2877fdeb12998aa6266e8e733796883814732ff
Khalil <Khalilp@brq.com>
2022-10-20 08:53:09 -0300

    Create search function in APICaller

--- NewsApp/API/APICaller.swift
+++ NewsApp/API/APICaller.swift
@@ -9,16 +9,37 @@ import Foundation
 
 enum NewsUrls: String {
     //TODO: - Remove APIKey from APICaller
-    case topHeadlines = "https://newsapi.org/v2/top-headlines?country=br&apiKey=da861279e5024fedbb825ccb49495edf"
+    case topHeadlines = "https://newsapi.org/v2/top-headlines?country=US&apiKey=da861279e5024fedbb825ccb49495edf"
+    case searchUrl = "https://newsapi.org/v2/everything?sortBy=popularity&apiKey=da861279e5024fedbb825ccb49495edf&q="
 }
 
 final class APICaller {
     static let sharedInstance = APICaller()
     
-    private init() {}
-    
+    //TODO: - Consolidate the functions(getTopStories and search)
     public func getTopStories(completion: @escaping  (Result<[Article], Error>) -> Void ) {
         guard let url = URL(string: NewsUrls.topHeadlines.rawValue) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    completion(.success(result.articles))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    public func search(with query: String, completion: @escaping  (Result<[Article], Error>) -> Void ) {
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        guard let url = URL(string: NewsUrls.searchUrl.rawValue + query) else { return }
         
         let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
             if let error = error {
--- NewsApp/Protocol/NewsAppLoadingProtocol.swift
+++ NewsApp/Protocol/NewsAppLoadingProtocol.swift
@@ -12,7 +12,7 @@ protocol NewsAppLoadingProtocol { }
 
 extension NewsAppLoadingProtocol where Self: UIViewController {
     func showLoadingIndicator() {
-        let alert = UIAlertController(title: nil, message: "Carregando...", preferredStyle: .alert)
+        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
         
         let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
         loadingIndicator.hidesWhenStopped = true
